e295e1956c836d0bdc4df44946434086e27e775c
Bogdan Artyushenko <bogdartysh@users.noreply.github.com>
2022-02-14 13:31:39 +0200

    Update SpringLogginProcessingExampleApplication.java

--- src/main/java/com/ba/logback/mask/example/SpringLogginProcessingExampleApplication.java
+++ src/main/java/com/ba/logback/mask/example/SpringLogginProcessingExampleApplication.java
@@ -40,5 +40,30 @@ public class SpringLogginProcessingExampleApplication {
         String pubdatastr;
         String inn;
     }
+    
+    @Builder
+    @Data
+    public static class Address {
+        @ToString.Include(name="street")
+        public String addressStreetToString() {
+            return toMaskedAddress(street);
+        }
+        String street;
+    }
 
+    
+    public static String toMaskedAddress(String str) {
+        return toStringFirstCharsOfEachWord(str, 1);
+    }
+    
+        static String toStringFirstCharsOfEachWord(String str, int firstChars) {
+        try {
+            return !StringUtils.isEmpty(str) && str.length() > firstChars ? (String)Arrays.stream(str.split("\\p{Space}")).map((s) -> {
+                return s.length() > firstChars ? s.substring(0, firstChars) + "*****" : s;
+            }).collect(Collectors.joining(" ")) : str;
+        } catch (Exception var3) {
+            LOG.warn("failed to mask [{}]", var3.getMessage(), var3);
+            return str;
+        }
+    }
 }
